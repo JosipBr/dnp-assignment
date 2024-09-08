436125a8ea0bd7056ecd2b0a8399f6450141f027
Josip Brljevic <josipbrljevic@gmail.com>
2024-09-08 18:22:18 +0200

    file fix again

---
+++ Server/Entity/Comment.cs
@@ -0,0 +1,10 @@
+namespace Entity;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Body { get; set; }
+    public int PostId { get; set; }
+    public int Likes { get; set; }
+    public int Dislikes { get; set; }
+}
--- Server/InMemoryRepositories/PostInMemoryRepository.cs
+++ Server/InMemoryRepositories/PostInMemoryRepository.cs
@@ -1,4 +1,4 @@
-using Entities;
+using Entity;
 using RepositoryContracts;
 
 namespace InMemoryRepositories;
